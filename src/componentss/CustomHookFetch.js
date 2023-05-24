06c70e8a440fa4a2c7d398b18ebf1df8d604e9d0
KellyD <kascenethde@gmail.com>
2023-05-24 10:53:27 -0500

    Actualización Hook para personalizar el fetch

--- src/componentss/CustomHookFetch.js
+++ src/componentss/CustomHookFetch.js
@@ -1,6 +1,14 @@
-function CustomHookFetch(url){
-    fetch(url)
-    .then( )
-    .catch
-    return "hola";
-}
+
+import {useEffect, useState} from "react";
+
+export const useFetch =(url) =>{
+    const [data, setData] =  useState(null);
+
+    useEffect(()=>{
+
+        fetch(url)
+        .then((res)=> res.json())
+        .then((data)=> setData(data))
+    }, [url]);
+    return [data];
+};
